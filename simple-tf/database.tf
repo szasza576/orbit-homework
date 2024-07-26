b435b5ecacfa8338319218b4d5fed7ecee10e8c5
Gabor Szabo <szasza576@gmail.com>
2024-07-26 10:55:35 +0200

    Add dependence to subnet

--- simple-tf/database.tf
+++ simple-tf/database.tf
@@ -19,7 +19,7 @@ resource "azurerm_postgresql_flexible_server" "Database" {
   storage_tier = "P4"
 
   sku_name   = "B_Standard_B1ms"
-  depends_on = [azurerm_private_dns_zone_virtual_network_link.DatabasePDNSLink]
+  depends_on = [azurerm_subnet.DatabaseSubnet, azurerm_private_dns_zone_virtual_network_link.DatabasePDNSLink]
 }
 
 resource "azurerm_postgresql_flexible_server_firewall_rule" "DatabaseFirewallRule" {
